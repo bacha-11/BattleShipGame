ceaf0eeaa5542a2f3a7fb05deb3aca522e80610b
bacha-11 <ijazbacha14@gmail.com>
2021-07-12 13:27:49 +0500

    create a contraller obj and then create parseGuess function

--- main.js
+++ main.js
@@ -70,7 +70,37 @@ var model = {
 };
 
 
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
+let controller = {
+    guesses: 0,
+
+    processGuess: function(guess){
+
+    },
+
+    parseGuess: function(guess){
+
+        let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+        if(guess === null || guess.length !== 2 ){
+            alert("Oops, please enter a letter and a number on the board!")
+        }
+        else{
+            let firstChar = guess.charAt(0)
+            let row = alphabet.indexOf(firstChar)
+            let column = guess.charAt(1)
+
+            if(isNaN(row) || isNaN(column)){
+                alert("Oops, that isn't on the board")
+
+            }else if( row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
+                alert("Oops, that's off the board!")
+
+            }else{
+                return row + column
+            }
+        }
+        return null
+    }
+};
+
+
